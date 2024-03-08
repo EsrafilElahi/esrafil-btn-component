732dbca5a8a0227d0aa7d72c280d3bc4ba356bb8
esrafilelahi <essinho1121@gmail.com>
2024-03-08 19:57:29 +0330

    change name entry

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "index.ts"),
-      name: "ViteButton",
+      name: "esrafilButton",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
