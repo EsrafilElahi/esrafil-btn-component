fd90a76caf0f291c5c1091f0fb13ba55224c3e6c
esrafilelahi <essinho1121@gmail.com>
2024-03-08 20:56:17 +0330

    use esrafil-btn-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
-// import { Button } from "esrafil-btn-component";
-import { Button } from "./components/Button";
+import { Button } from "esrafil-btn-component";
 
 function App() {
   return (
